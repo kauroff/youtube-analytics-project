18fa6617ec55a597783300952fcb02949f1bee5b
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-10-11 23:18:53 +0300

    Edited fifth homework

--- src/playlist.py
+++ src/playlist.py
@@ -18,7 +18,7 @@ class PlayList:
         self.url = f'https://www.youtube.com/playlist?list={url}'
 
     def total_duration(self):
-        return 0
+        sum(2, 3)
 
     def show_best_video(self):
         pass
